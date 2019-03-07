8d4daab8e85093e2f3761d3a2d694c74ec122e23
joshzwiebel <jjzwiebe@edu.uwaterloo.ca>
2019-03-06 22:11:10 -0500

    Thouroughly commented the code
    Feeling real good about this one boyz

--- main.cpp
+++ main.cpp
@@ -2,32 +2,34 @@
 
 using namespace std;
 
+/* the insight in this problem was that Queues were unnecessary as you were always progressing through the list linearly and as long as you keep track of the rounds as they pass
+ * there is no reason to move around items but it is cleaner and easier to keep track of indices and rounds as they pass.
+ * This makes the complexity O(n) as you are only ever counting over as many numbers
+ * as you need to add and there are no wasted passes or operations*/
 int ramenChallenge(int rounds, int space, int *groups, int number_of_groups) {
-    int temp_sum = 0;
-    int sum = 0;
-    int index = 0;
-    int round_counter = 0;
-    int index_counter = 0;
+    int temp_sum = 0; // this will keep track of the temporary sum. ex: the numbers we add in a given round/group size before the line for ramen fills
+    int sum = 0; // this is the sum of the money the ramen bar makes at 1 dollar per person
+    int index = 0; // this is the index in the list that we iterate through
+    int round_counter = 0; // this will keep track of how many calls to ramen have passed. i.e the rounds or R value
+    int index_counter = 0; // this will keep track of the number of times the index is incremented as the same person cannot eat twice in a round
     while (round_counter < rounds) {
-        //todo change the algorithm to simplify the conditionals. use a variable but with less if statements
-        //todo account for case where the same person cannot eat 2x in one round
-        sum += groups[index];
-        temp_sum += groups[index];
-        if(temp_sum>space||index_counter==number_of_groups){
-            temp_sum = groups[index];
-            round_counter++;
-            index_counter=0;
+        sum += groups[index]; // add the first group to the money sum
+        temp_sum += groups[index]; // the temporary sum is updated as well
+        if (temp_sum > space || index_counter == number_of_groups) { // to prevent the same person from eating 2x or more or overfilling  we use a conditional
+            temp_sum = groups[index]; // the temporary sum is set to the index that we are currently adding
+            round_counter++; // the call to ramen is complete the the round counter is incremented
+            index_counter = 0; //since the round is complete everyone can eat again so the index counter is reset
         }
-        if(index <number_of_groups-1)
+        if (index < number_of_groups - 1) //this if else block is simply to prevent accessing memory beyond the array and keep the index looping
             index++;
         else
-            index=0;
-        index_counter++;
+            index = 0;
+        index_counter++;// incremented after every pass
     }
-    if(index!=0)
-        sum-=groups[index-1];
+    if (index != 0) //this if else subtracts the last block that was added as the final pass is technically an overfilled line so must be corrected for
+        sum -= groups[index - 1];
     else
-        sum-=groups[number_of_groups-1];
+        sum -= groups[number_of_groups - 1];
     return sum;
 }
 
@@ -35,13 +37,13 @@ int main() {
     int array1[10] = {2, 4, 2, 3, 4, 2, 1, 2, 1, 3};
     int array2[1] = {1};
     int array3[4] = {1, 4, 2, 1};
-    int array4[2] = {1,2};
-    int array5[11]= {43,34,39,11,22,35,46,59,16,13,34};
+    int array4[2] = {1, 2};
+    int array5[11] = {43, 34, 39, 11, 22, 35, 46, 59, 16, 13, 34};
 
     cout << ramenChallenge(4, 6, array3, 4) << endl;
     cout << ramenChallenge(100, 10, array2, 1) << endl;
     cout << ramenChallenge(5, 5, array1, 10) << endl;
-    cout << ramenChallenge(100000000, 2, array4, 2) << endl;
+    cout << ramenChallenge(100000000, 2, array4, 2) << endl; //as you can see the algorithm is blazing quick
     cout << ramenChallenge(1000000, 200, array5, 11) << endl;
 
 
